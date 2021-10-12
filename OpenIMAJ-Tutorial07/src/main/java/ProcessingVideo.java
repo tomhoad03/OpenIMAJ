17c2caf9014af4fca3c07b5b619f2046893b9fde
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2021-10-12 12:34:38 +0100

    Chapter 7 - processing video

---
+++ OpenIMAJ-Tutorial07/src/main/java/ProcessingVideo.java
@@ -0,0 +1,42 @@
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.processing.edges.CannyEdgeDetector;
+import org.openimaj.video.Video;
+import org.openimaj.video.VideoDisplay;
+import org.openimaj.video.VideoDisplayListener;
+import org.openimaj.video.capture.VideoCapture;
+import org.openimaj.video.xuggle.XuggleVideo;
+
+import java.net.URL;
+
+public class ProcessingVideo {
+    public static void main( String[] args ) {
+        try {
+            // gets a video using Xuggle library
+            Video<MBFImage> video = new XuggleVideo(new URL("http://static.openimaj.org/media/tutorial/keyboardcat.flv"));
+            VideoDisplay<MBFImage> display1 = VideoDisplay.createVideoDisplay(video);
+
+            // can get video from the webcam
+            video = new VideoCapture(320, 240);
+
+            // displays the video as a series of images and processes the images
+            for (MBFImage mbfImage : video) {
+                DisplayUtilities.displayName(mbfImage.process(new CannyEdgeDetector()), "videoFrames");
+            }
+
+            // an alternate approach using an event driven technique
+            VideoDisplay<MBFImage> display2 = VideoDisplay.createVideoDisplay(video);
+            display2.addVideoListener(
+                    new VideoDisplayListener<>() {
+                        public void beforeUpdate(MBFImage frame) {
+                            frame.processInplace(new CannyEdgeDetector());
+                        }
+
+                        public void afterUpdate(VideoDisplay<MBFImage> display) {
+                        }
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
