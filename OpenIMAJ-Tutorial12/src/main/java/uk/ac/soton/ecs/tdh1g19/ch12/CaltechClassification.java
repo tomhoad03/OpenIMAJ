a35ec475b3260a1ff8e4a0f97dc39d0bf0b174ea
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2021-11-22 16:30:47 +0000

    Chapter walkthroughs

--- OpenIMAJ-Tutorial12/src/main/java/uk/ac/soton/ecs/tdh1g19/ch12/CaltechClassification.java
+++ OpenIMAJ-Tutorial12/src/main/java/uk/ac/soton/ecs/tdh1g19/ch12/CaltechClassification.java
@@ -1,25 +1,118 @@
 package uk.ac.soton.ecs.tdh1g19.ch12;
 
+import de.bwaldvogel.liblinear.SolverType;
+import org.openimaj.data.DataSource;
+import org.openimaj.data.dataset.Dataset;
 import org.openimaj.data.dataset.GroupedDataset;
 import org.openimaj.data.dataset.ListDataset;
 import org.openimaj.data.dataset.VFSListDataset;
 import org.openimaj.experiment.dataset.sampling.GroupSampler;
+import org.openimaj.experiment.dataset.sampling.GroupedUniformRandomisedSampler;
 import org.openimaj.experiment.dataset.split.GroupedRandomSplitter;
+import org.openimaj.experiment.evaluation.classification.ClassificationEvaluator;
+import org.openimaj.experiment.evaluation.classification.ClassificationResult;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMAnalyser;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMResult;
+import org.openimaj.feature.DoubleFV;
+import org.openimaj.feature.FeatureExtractor;
+import org.openimaj.feature.SparseIntFV;
+import org.openimaj.feature.local.data.LocalFeatureListDataSource;
+import org.openimaj.feature.local.list.LocalFeatureList;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.annotation.evaluation.datasets.Caltech101;
 import org.openimaj.image.annotation.evaluation.datasets.Caltech101.Record;
+import org.openimaj.image.feature.dense.gradient.dsift.ByteDSIFTKeypoint;
+import org.openimaj.image.feature.dense.gradient.dsift.DenseSIFT;
+import org.openimaj.image.feature.dense.gradient.dsift.PyramidDenseSIFT;
+import org.openimaj.image.feature.local.aggregate.BagOfVisualWords;
+import org.openimaj.image.feature.local.aggregate.BlockSpatialAggregator;
+import org.openimaj.ml.annotation.linear.LiblinearAnnotator;
+import org.openimaj.ml.clustering.ByteCentroidsResult;
+import org.openimaj.ml.clustering.assignment.HardAssigner;
+import org.openimaj.ml.clustering.kmeans.ByteKMeans;
+import org.openimaj.util.pair.IntFloatPair;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class CaltechClassification {
     public static void main(String[] args) {
         try {
+            // Get the Caltech dataset
             GroupedDataset<String, VFSListDataset<Record<FImage>>, Record<FImage>> allData = Caltech101.getData(ImageUtilities.FIMAGE_READER);
 
+            // Take a subset of this database
             GroupedDataset<String, ListDataset<Record<FImage>>, Record<FImage>> data = GroupSampler.sample(allData, 5, false);
 
+            // Split the subset into training and testing sets
             GroupedRandomSplitter<String, Record<FImage>> splits = new GroupedRandomSplitter<>(data, 15, 0, 15);
+
+            // Create SIFT extractor
+            DenseSIFT dsift = new DenseSIFT(5, 7);
+            PyramidDenseSIFT<FImage> pdsift = new PyramidDenseSIFT<>(dsift, 6f, 7);
+
+            // Clusterer and feature extractor
+            HardAssigner<byte[], float[], IntFloatPair> assigner = trainQuantiser(GroupedUniformRandomisedSampler.sample(splits.getTrainingDataset(), 30), pdsift);
+            FeatureExtractor<DoubleFV, Record<FImage>> extractor = new PHOWExtractor(pdsift, assigner);
+
+            // Construct and train a classifier
+            LiblinearAnnotator<Record<FImage>, String> ann = new LiblinearAnnotator<>(extractor, LiblinearAnnotator.Mode.MULTICLASS, SolverType.L2R_L2LOSS_SVC, 1.0, 0.00001);
+            ann.train(splits.getTrainingDataset());
+
+            // Evaluate the classification accuracy
+            ClassificationEvaluator<CMResult<String>, String, Record<FImage>> eval = new ClassificationEvaluator<>(ann, splits.getTestDataset(), new CMAnalyser<>(CMAnalyser.Strategy.SINGLE));
+            Map<Record<FImage>, ClassificationResult<String>> guesses = eval.evaluate();
+            CMResult<String> result = eval.analyse(guesses);
+
+            System.out.println(result);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    // K-means clustering
+    static HardAssigner<byte[], float[], IntFloatPair> trainQuantiser(Dataset<Record<FImage>> sample, PyramidDenseSIFT<FImage> pdsift) {
+        List<LocalFeatureList<ByteDSIFTKeypoint>> allkeys = new ArrayList<>();
+
+        for (Record<FImage> rec : sample) {
+            FImage img = rec.getImage();
+
+            pdsift.analyseImage(img);
+            allkeys.add(pdsift.getByteKeypoints(0.005f));
+        }
+
+        if (allkeys.size() > 10000)
+            allkeys = allkeys.subList(0, 10000);
+
+        ByteKMeans km = ByteKMeans.createKDTreeEnsemble(300);
+        DataSource<byte[]> datasource = new LocalFeatureListDataSource<>(allkeys);
+        ByteCentroidsResult result = km.cluster(datasource);
+
+        return result.defaultHardAssigner();
+    }
+
+    // Feature extraction
+    static class PHOWExtractor implements FeatureExtractor<DoubleFV, Record<FImage>> {
+        PyramidDenseSIFT<FImage> pdsift;
+        HardAssigner<byte[], float[], IntFloatPair> assigner;
+
+        public PHOWExtractor(PyramidDenseSIFT<FImage> pdsift, HardAssigner<byte[], float[], IntFloatPair> assigner)
+        {
+            this.pdsift = pdsift;
+            this.assigner = assigner;
+        }
+
+        public DoubleFV extractFeature(Record<FImage> object) {
+            FImage image = object.getImage();
+            pdsift.analyseImage(image);
+
+            BagOfVisualWords<byte[]> bovw = new BagOfVisualWords<>(assigner);
+
+            BlockSpatialAggregator<byte[], SparseIntFV> spatial = new BlockSpatialAggregator<>(bovw, 2, 2);
+
+            return spatial.aggregate(pdsift.getByteKeypoints(0.015f), image.getBounds()).normaliseFV();
         }
     }
 }
--- OpenIMAJ-Tutorial14/src/main/java/uk/ac/soton/ecs/tdh1g19/ch14/ParallelProcessing.java
+++ OpenIMAJ-Tutorial14/src/main/java/uk/ac/soton/ecs/tdh1g19/ch14/ParallelProcessing.java
@@ -1,17 +1,71 @@
 package uk.ac.soton.ecs.tdh1g19.ch14;
 
+import org.openimaj.data.dataset.GroupedDataset;
+import org.openimaj.data.dataset.ListDataset;
 import org.openimaj.data.dataset.VFSGroupDataset;
+import org.openimaj.experiment.dataset.sampling.GroupSampler;
+import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
 import org.openimaj.image.annotation.evaluation.datasets.Caltech101;
+import org.openimaj.image.colour.ColourSpace;
+import org.openimaj.image.colour.RGBColour;
+import org.openimaj.image.processing.resize.ResizeProcessor;
+import org.openimaj.time.Timer;
 import org.openimaj.util.parallel.Parallel;
+import org.openimaj.util.parallel.partition.RangePartitioner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParallelProcessing {
     public static void main(String[] args) {
         try {
             Parallel.forIndex(0, 10, 1, System.out::println);
 
+            // Get the Caltech database
             VFSGroupDataset<MBFImage> allImages = Caltech101.getImages(ImageUtilities.MBFIMAGE_READER);
+
+            // Get a sample of the dataset
+            GroupedDataset<String, ListDataset<MBFImage>, MBFImage> images = GroupSampler.sample(allImages, 8, false);
+
+            // Image processing
+            List<MBFImage> output = new ArrayList<>();
+            ResizeProcessor resize = new ResizeProcessor(200);
+            Timer t1 = Timer.timer();
+
+            // Parallel processing - exercise 1
+            for (ListDataset<MBFImage> clzImages : images.values()) {
+                final MBFImage current = new MBFImage(200, 200, ColourSpace.RGB);
+
+                Parallel.forEachPartitioned(new RangePartitioner<>(clzImages), it -> {
+                    MBFImage tmpAccum = new MBFImage(200, 200, 3);
+                    MBFImage tmp = new MBFImage(200, 200, ColourSpace.RGB);
+
+                    while (it.hasNext()) {
+                        final MBFImage i = it.next();
+                        tmp.fill(RGBColour.WHITE);
+
+                        final MBFImage small = i.process(resize).normalise();
+                        final int x = (200 - small.getWidth()) / 2;
+                        final int y = (200 - small.getHeight()) / 2;
+                        tmp.drawImage(small, x, y);
+                        tmpAccum.addInplace(tmp);
+                    }
+                    synchronized (current) {
+                        current.addInplace(tmpAccum);
+                    }
+                });
+                current.divideInplace((float) clzImages.size());
+                output.add(current);
+            }
+
+            DisplayUtilities.display("Images", output);
+            System.out.println("Time: " + t1.duration() + "ms");
+
+            // 15409ms - no parallelization
+            // 4545ms - parallelization of the inner loop
+            // 4400ms - improved parallelization of the inner loop
         } catch (Exception e) {
             e.printStackTrace();
         }
